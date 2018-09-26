70d33f0abbe870da73d39147d7439aff80599a56
若云 <zyhdvlp@gmail.com>
2018-09-26 10:45:50 +0800

    URLSession

--- thread_exercise/07Http/NSURLSessionViewController.m
+++ thread_exercise/07Http/NSURLSessionViewController.m
@@ -27,7 +27,7 @@
  */
 #import "NSURLSessionViewController.h"
 
-@interface NSURLSessionViewController () <NSURLSessionDataDelegate>
+@interface NSURLSessionViewController () <NSURLSessionDataDelegate, NSURLSessionDownloadDelegate>
 
 @end
 
@@ -37,7 +37,9 @@
 //    [self get];
 //    [self get2];
 //    [self post];
-    [self delegate];
+//    [self delegate];
+//    [self downloadWithBlock];
+    [self downloadWithDelegate];
 }
 
 - (void)get {
@@ -203,6 +205,115 @@
     NSLog(@"%@", [NSThread currentThread]);//主线程
 
 }
+
+//优点:不需要担心内存
+//缺点:无法监听文件下载进度
+- (void)downloadWithBlock {
+    //1.url
+    NSURL *url = [NSURL URLWithString:@"http://img02.tooopen.com/images/20160509/tooopen_sy_161967094653.jpg"];
+
+    //2.创建请求对象
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    //3.创建session
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    //4.创建Task
+    /*
+     第一个参数:请求对象
+     第二个参数:completionHandler 回调
+        location:
+        response:响应头信息
+        error:错误信息
+     */
+    //该方法内部已经实现了边接受数据边写沙盒(tmp)的操作
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request completionHandler:^(NSURL *_Nullable location, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+        if (error) {
+            NSLog(@"发生错误");
+            return;
+        }
+
+        //6.处理数据
+        NSLog(@"%@---%@", location, [NSThread currentThread]);//子线程
+
+        //6.1 拼接文件全路径
+        NSString *fullPath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:response.suggestedFilename];
+
+        //6.2 剪切文件
+        [[NSFileManager defaultManager] moveItemAtURL:location toURL:[NSURL fileURLWithPath:fullPath] error:nil];
+        NSLog(@"%@", fullPath);
+    }];
+
+    //5.执行Task
+    [downloadTask resume];
+}
+
+- (void)downloadWithDelegate {
+    //1.url
+    NSURL *url = [NSURL URLWithString:@"http://img02.tooopen.com/images/20160509/tooopen_sy_161967094653.jpg"];
+
+    //2.创建请求对象
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    //3.创建session
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+
+    //4.创建Task
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request];
+
+    //5.执行Task
+    [downloadTask resume];
+}
+
+#pragma mark ----------------------
+#pragma mark NSURLSessionDownloadDelegate
+
+/**
+ *  写数据
+ *
+ *  @param session                   会话对象
+ *  @param downloadTask              下载任务
+ *  @param bytesWritten              本次写入的数据大小
+ *  @param totalBytesWritten         下载的数据总大小
+ *  @param totalBytesExpectedToWrite  文件的总大小
+ */
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    //1. 获得文件的下载进度
+    NSLog(@"%f", 1.0 * totalBytesWritten / totalBytesExpectedToWrite);
+}
+
+/**
+ *  当恢复下载的时候调用该方法
+ *
+ *  @param fileOffset         从什么地方下载
+ *  @param expectedTotalBytes 文件的总大小
+ */
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes {
+    NSLog(@"%s", __func__);
+}
+
+/**
+ *  当下载完成的时候调用
+ *
+ *  @param location     文件的临时存储路径
+ */
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    NSLog(@"%@", location);
+
+    //1 拼接文件全路径
+    NSString *fullPath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:downloadTask.response.suggestedFilename];
+
+    //2 剪切文件
+    [[NSFileManager defaultManager] moveItemAtURL:location toURL:[NSURL fileURLWithPath:fullPath] error:nil];
+    NSLog(@"%@", fullPath);
+}
+
+/**
+ *  请求结束
+ */
+//- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+//    NSLog(@"didCompleteWithError");
+//}
 
 
 @end
